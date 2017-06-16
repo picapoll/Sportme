9225724ec42694ca351682f163d8924cebbb6553
Bernat Moragas <bmoragas@gmail.com>
2017-06-16 14:40:33 +0200

    Title

--- public/js/autocomplete.js
+++ public/js/autocomplete.js
@@ -10,7 +10,6 @@ var website
 var photoId
 var photoWeb
 
-
 google.maps.event.addDomListener(window, 'load', function () {
   const places = new google.maps.places.Autocomplete(document.getElementById('txtPlaces'))
   google.maps.event.addListener(places, 'place_changed', function () {
@@ -18,20 +17,10 @@ google.maps.event.addDomListener(window, 'load', function () {
     address = place.formatted_address
     lat = place.geometry.location.lat().toFixed(4)
     lon = place.geometry.location.lng().toFixed(4)
-    gwww =place.url
+    gwww = place.url
     phone = place.formatted_phone_number
-    icon=place.icon
-    // photoId =place.photos.photo_reference
+    icon = place.icon
     website = place.website
-    photoWeb ='https:'+'//'+ 'maps.googleapis.com/maps/api/place/photo?' + photoId 
-
-    console.log(place.geometry.location.lat(), place.geometry.location.lng())
-    console.log('Google Maps API version: ' + google.maps.version)
-
-        console.log(photoId)
-         console.log(photoWeb)
-
-        console.log(gwww)
+    photoWeb = 'https:' + '//' + 'maps.googleapis.com/maps/api/place/photo?' + photoId
   })
 })
-  
